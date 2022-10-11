34b2bce4877ca9d917e315dd5e5ecd7c22b24f71
adithyas2000 <adithyasaranga@gmail.com>
2022-10-12 00:02:35 +0530

    Added player registration for trophy

---
+++ ESports/Data/AppDbContext.cs
@@ -0,0 +1,15 @@
+﻿using ESports.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace ESports.Data
+{
+    public class AppDbContext:DbContext
+    {
+        public AppDbContext(DbContextOptions<AppDbContext> options):base(options)
+        {
+            
+        }
+        public DbSet<Player> Players { get; set; }
+        public DbSet<TrophyRegistration> TrophyRegistrations { get; set; }
+    }
+}
--- ESports/Program.cs
+++ ESports/Program.cs
@@ -1,7 +1,11 @@
+using ESports.Data;
+using Microsoft.EntityFrameworkCore;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 builder.Services.AddControllersWithViews();
+builder.Services.AddDbContext<AppDbContext>(options => options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultString")));
 
 var app = builder.Build();
 
