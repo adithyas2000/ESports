ef297c89b408e0de1f1d75d09868a88b84bc8336
jinaliApium <jinali@apium.io>
2022-10-11 23:27:13 +0530

    Added Identity ad=nd Tropy create and view list

---
+++ ESports/Models/Trophy.cs
@@ -0,0 +1,34 @@
+﻿using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
+
+namespace ESports.Models
+{
+    public class Trophy
+    {
+        [Key]
+        public int Id { get; set; }
+
+
+        [Required]
+        [DisplayName("Trophy Name")]
+        public string Name { get; set; }
+
+        [Required]
+        [DisplayName("Event Venue")]
+        public string Venue { get; set; }
+
+        [Required]
+        [DisplayName("Event Starting Date")]
+        public DateTime StartDate { get; set; }
+
+        [Required]
+        [DisplayName("Event Ending Date")]
+        public DateTime EndDate { get; set; }
+
+        [Required]
+        [DisplayName("Registration/Bidding Closing Date")]
+        public DateTime ClosingDate { get; set; } 
+
+        public DateTime CreatedDateTime { get; set; } = DateTime.Now;
+    }
+}
--- ESports/Program.cs
+++ ESports/Program.cs
@@ -1,7 +1,22 @@
+using Microsoft.AspNetCore.Identity;
+using Microsoft.EntityFrameworkCore;
+using ESports.Areas.Identity.Data;
+using ESports.Data;
+
 var builder = WebApplication.CreateBuilder(args);
+var connectionString = builder.Configuration.GetConnectionString("ApplicationDbContextConnection") ?? throw new InvalidOperationException("Connection string 'ApplicationDbContextConnection' not found.");
+
+builder.Services.AddDbContext<ApplicationDbContext>(options =>
+    options.UseSqlServer(connectionString));
+
+builder.Services.AddDefaultIdentity<ApplicationUser>(options => options.SignIn.RequireConfirmedAccount = false)
+    .AddRoles<IdentityRole>()
+    .AddEntityFrameworkStores<ApplicationDbContext>();
 
 // Add services to the container.
 builder.Services.AddControllersWithViews();
+builder.Services.AddDbContext<AppDbContext>(options => options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection")));
+
 
 var app = builder.Build();
 
@@ -17,11 +32,13 @@ app.UseHttpsRedirection();
 app.UseStaticFiles();
 
 app.UseRouting();
+app.UseAuthentication();;
 
 app.UseAuthorization();
 
 app.MapControllerRoute(
     name: "default",
     pattern: "{controller=Home}/{action=Index}/{id?}");
-
+//to get razor page support
+app.MapRazorPages();
 app.Run();
