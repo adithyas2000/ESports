94dba1acabad1ac2f7eb7c029c396b290bfac743
adithyas2000 <adithyasaranga@gmail.com>
2022-10-13 00:29:08 +0530

    AD6 Copy

---
+++ ESports/Controllers/PlayerController.cs
@@ -0,0 +1,31 @@
+﻿using ESports.Data;
+using ESports.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace ESports.Controllers
+{
+    public class PlayerController : Controller
+    {
+        private readonly AppDbContext _db;
+        public PlayerController(AppDbContext db)
+        {
+            _db = db;
+        }
+        public IActionResult Index()
+        {
+            var playerList = _db.TrophyRegistrations.Where(a=>a.CurrentTeam=="NULL").ToList();
+            if (playerList != null)
+            {
+                return View(playerList);
+
+            }
+            return RedirectToAction("Index","Trophy");
+        }
+
+        public IActionResult Profile(string nic)
+        {
+            var player = _db.TrophyRegistrations.Where(a => a.PlayerNIC == nic).ToList();
+            return View(player[0]);
+        }
+    }
+}
